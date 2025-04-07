28c7897b25231428031f286367097c387a6851df
TylerMcKenna <101288310+TylerMcKenna@users.noreply.github.com>
2025-04-07 11:59:36 -0500

    Add some styling

--- scripts.js
+++ scripts.js
@@ -1,24 +1,26 @@
 async function callAPI(location, date1, date2) {
     const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${date1}/${date2}?unitGroup=us&include=days&elements=conditions,datetime,temp,windgust,windspeed,description,resolvedAddress&key=FNWD53K7LC58RZX68HC4QJCNV`;
-    try {
-        const response = await fetch(url, {mode: "cors"});
+    const response = await fetch(url, {mode: "cors"});
 
-        if (!response.ok) {
-            throw new Error(`Response Status: ${response.status}`);
-        }
-
+    if (response.status !== 200) {
+        throw new Error(`Error Status: ${response.status}`);
+    } else {
         const json = await response.json();
         return json;
-    } catch (error) {
-        throw error;
     }
 }
 
 async function getWeather(location, date1 = "", date2 = "") {
-    if (date1 === "") {
-        const today = new Date();
-        date1 = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
+    try {
+        // default date1 to the current day otherwise the range would be the next 15 days
+        if (date1 === "") {
+            const today = new Date();
+            date1 = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
+        }
+        const weather = await callAPI(location, date1, date2);
+        document.querySelector("#infoDisplay").textContent = "Query complete!";
+        console.log(weather);
+    } catch (error) {
+        console.log(`Fetching weather with location "${location}", startDate "${date1}", and endDate "${date2}" failed: \n\t ${error.message}`);
     }
-    const weather = await callAPI(location, date1, date2);
-    console.log(weather);
 }
