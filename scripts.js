e5cd747899d925893954b3e907c2711824be5200
TylerMcKenna <101288310+TylerMcKenna@users.noreply.github.com>
2025-04-09 23:23:32 -0500

    Data Populates (Mostly) Properly on Weather Page
    Forecast doesn't populate but is very close.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 async function callAPI(location, date1, date2) {
     const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${date1}/${date2}?unitGroup=us&include=days&elements=conditions,datetime,temp,windgust,windspeed,description,resolvedAddress&key=FNWD53K7LC58RZX68HC4QJCNV`;
+    console.log(url);
     const response = await fetch(url, {mode: "cors"});
 
     if (response.status !== 200) {
@@ -10,17 +11,51 @@ async function callAPI(location, date1, date2) {
     }
 }
 
-async function getWeather(location, date1 = "", date2 = "") {
-    try {
-        // default date1 to the current day otherwise the range would be the next 15 days
-        if (date1 === "") {
-            const today = new Date();
-            date1 = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
-        }
-        const weather = await callAPI(location, date1, date2);
-        document.querySelector("#infoDisplay").textContent = "Query complete!";
+// Want to let users choose dates but for now, not doing that
+async function getWeather(location) {
+    // Today, at the start of the day
+    // This is neccessary to use Epoch time, which is cleaner.
+    // If we do not go to the start of the day we may have our 
+    // day round up to tomorrow.
+    const now = new Date();
+    now.setHours(0, 0, 0, 0)
+
+    // 6.048e8 is a week in milliseconds
+    const today = Math.floor(now.getTime() / 1000);
+    const weekFromToday = Math.floor((now.getTime() + 6.048e8) / 1000);
+
+    try {    
+        const weather = await callAPI(location, today, weekFromToday);
         console.log(weather);
+        return weather;
     } catch (error) {
-        console.log(`Fetching weather with location "${location}", startDate "${date1}", and endDate "${date2}" failed: \n\t ${error.message}`);
+        console.log(`Fetching weather with location "${location}", startDate "${today}", and endDate "${weekFromToday}" failed: \n\t ${error.message}`);
     }
 }
+
+function updateWeatherPage(weather) {
+    // Sets current day's information
+    const { address, resolvedAddress, days } = weather;
+    
+    const otherAddressInfo = resolvedAddress.slice(resolvedAddress.indexOf(",") + 2);
+    const today = days[0];
+    document.querySelector(".city").textContent = address;
+    document.querySelector(".otherLocationDetails").textContent = otherAddressInfo;
+    document.querySelector(".currentTemp").textContent = `${Math.round(today["temp"])}°`;
+
+    // Remove today since we don't need it anymore
+    days.splice(0, 1);
+
+    for (let i = 0; i < days.length; i++) {
+        const dayElement = document.querySelector(`.day${i}`);
+        console.log(dayElement);                              
+    }
+}
+
+async function main() {
+    const weather = await getWeather("London");
+    updateWeatherPage(weather);
+    console.log(Date(Date.now()).get);
+}
+
+main();
