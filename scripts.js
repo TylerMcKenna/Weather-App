38d62bea18fc3d466794dd648eaed77a52696aec
TylerMcKenna <101288310+TylerMcKenna@users.noreply.github.com>
2025-04-10 19:13:34 -0500

    Data all populating, need a way to choose cities

--- scripts.js
+++ scripts.js
@@ -2,7 +2,8 @@ const DAY_NAMES = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Frid
 const MONTH_NAMES = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "November", "October", "December"];
 
 async function callAPI(location, date1, date2) {
-    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${date1}/${date2}?unitGroup=us&include=days&elements=conditions,datetime,tempmax,tempmin,temp,windgust,windspeed,description,resolvedAddress&key=FNWD53K7LC58RZX68HC4QJCNV`;
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${date1}/${date2}?unitGroup=us&include=days&elements=conditions,datetime,tempmax,tempmin,temp,windgust,windspeed,description,resolvedAddress,preciptype,cloudcover&key=FNWD53K7LC58RZX68HC4QJCNV`;
+    console.log(url);
     const response = await fetch(url, {mode: "cors"});
 
     if (response.status !== 200) {
@@ -43,6 +44,20 @@ function updateWeatherPage(weather) {
     document.querySelector(".city").textContent = address;
     document.querySelector(".otherLocationDetails").textContent = otherAddressInfo;
     document.querySelector(".currentTemp").textContent = `${Math.round(today["temp"])}°`;
+    const precipitationType = today["preciptype"]
+    let icon = "sun";
+    if (today["cloudcover"] >= 70) {
+        icon = "cloud"
+    }
+    if (precipitationType !== null) {
+        // if rain is somewhere in the string
+        if (precipitationType.indexOf("rain") !== -1) {
+            icon = "rain"
+        } else {
+            icon = precipitationType;
+        }
+    }
+    document.querySelector(`.mainIcon`).src = `./images/${icon}.png`
 
     // Remove today since we don't need it anymore
     days.splice(0, 1);
@@ -52,23 +67,42 @@ function updateWeatherPage(weather) {
         const currentDay = days[i];
 
         const date = new Date(currentDay["datetime"]);
-        console.log(date);
         const dayOfTheWeek = DAY_NAMES[date.getDay()];
         const monthAndDay = `${MONTH_NAMES[date.getMonth()]} ${date.getUTCDate()}`
         const high = `High: ${Math.round(currentDay["tempmax"])}°`;
         const low = `Low: ${Math.round(currentDay["tempmin"])}°`;
         
         const properties = ["dayOfTheWeek", "monthAndDay", "high", "low"];
-        const values = [dayOfTheWeek, monthAndDay, high, low]
+        const values = [dayOfTheWeek, monthAndDay, high, low];
         let j = 0;
         for (const property of properties) {
             document.querySelector(`.${elementClasses} .${property}`).textContent = values[j++];              
         }
+
+        // Defaulting to either sunny or cloudy
+        let icon = "sun";
+        if (currentDay["cloudcover"] >= 70) {
+            icon = "cloud";
+        }
+
+        // Changing if we have precipitation
+        const precipitationType = currentDay["preciptype"];
+        console.log(precipitationType);
+        if (precipitationType !== null) {
+            // if rain is somewhere in the string
+            if (precipitationType.indexOf("rain") !== -1) {
+                icon = "rain"
+            } else {
+                icon = precipitationType;
+            }
+        }
+
+        document.querySelector(`.${elementClasses} .icon`).src = `./images/${icon}.png`
     }
 }
 
 async function main() {
-    const weather = await getWeather("London");
+    const weather = await getWeather("Dallas");
     updateWeatherPage(weather);
     console.log(Date(Date.now()).get);
 }
