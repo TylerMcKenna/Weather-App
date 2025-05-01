e43c500efc731e90910551840331f6896a5ddc8d
TylerMcKenna <101288310+TylerMcKenna@users.noreply.github.com>
2025-04-30 20:25:50 -0500

    Trying to fix it

--- server.js
+++ server.js
@@ -16,36 +16,43 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "pages", "index.html"));
 });
 
-app.post("/weatherPage", (req, res) => {
+app.post("/weatherPage", async (req, res) => {
     const city = req.body.city;
-
-    if (!city) {
+    
+    // Today, at the start of the day
+    // setHours(0,0,0,0) is neccessary to use Epoch time, which is easier to manipulate.
+    // If we do not reset the hours of the day, we end up getting the wrong day sometimes.
+    const now = new Date();
+    now.setHours(0, 0, 0, 0)
+    
+    // 6.048e8 is a week in milliseconds
+    const today = Math.floor(now.getTime() / 1000);
+    const weekFromToday = Math.floor((now.getTime() + 6.048e8) / 1000);
+    
+    try {
+        const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/${today}/${weekFromToday}?unitGroup=us&include=days&elements=conditions,datetime,tempmax,tempmin,temp,windgust,windspeed,description,resolvedAddress,preciptype,cloudcover&key=${process.env.API_KEY}`;
+        const requestContent = await fetch(url, {mode: "cors"});
+        if (requestContent) {
+            const json = await requestContent.json();
+            req.session.cityInfo = json; 
+        }
+    } catch (error) {
+        // console.log(`Error Status: ${requestContent.status} \n Error: ${error}`);
         res.redirect("/");
     }
-
-    req.session.city = city
     res.redirect("/weatherPage");
 })
 
 app.get("/weatherPage", (req, res) => {
-    if (!req.session.city) {
+    if (!req.session.cityInfo) {
         res.redirect("/");
     }
 
     res.sendFile(path.join(__dirname, "pages", "weather.html"));
-})
+});
 
-// anyone can request to this endpoint - probably not good
-app.get("/weather/:location/:date1/:date2", async (req, res) => {
-     try {
-          const { location, date1, date2 } = req.params;
-          const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${req.session.city}/${date1}/${date2}?unitGroup=us&include=days&elements=conditions,datetime,tempmax,tempmin,temp,windgust,windspeed,description,resolvedAddress,preciptype,cloudcover&key=${process.env.API_KEY}`;
-          const requestContent = await fetch(url, {mode: "cors"});
-          const json = await requestContent.json();
-          res.send(json)
-     } catch (error) {
-          console.log(`Error Status: ${requestContent.status} \n Error: ${error}`);
-     }
+app.get("/cityInfo", (req, res) => {
+    res.send(req.session.cityInfo);
 });
 
 const PORT = process.env.PORT || 3000;
